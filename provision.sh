fca86917cfccbeb79811b8ff46df3acbaf92bc9d
abel <abel.wang@gmail.com>
2019-06-12 10:40:25 -0700

    getting parameters from environmental variables

--- provision.sh
+++ provision.sh
@@ -1,210 +1,238 @@
-# This processes all the command line arguments and sets them to the appropriate variable so
-# all variables will be ready to be used in the rest of the script
+# The variables in pipelines are set as environment variables. This gets those environment
+# variables and sets them as local variables ready to be used in the rest of the script
 #
-echo "Checking environment variable for resource group name: $RESOURCEGROUPNAME"
-echo "Iterating through parameters..."
-echo "Number of parameters passed in: $#"
-foundResourceGroupName=false
-foundSubscriptionName=false
-foundResourceGroupRegion=false
-foundStorageAccountName=false
-foundStorageAccountRegion=false
-foundStorageAccountSku=false
-foundErrorDocumentName=false
-foundIndexDocumentName=false
-foundCosmosAccountName=false
-foundCosmosRegion=false
-foundCosmosDbName=false
-foundCosmosContainerName=false
-foundCosmosThroughput=false
-foundFunctionStorageAccountRegion=false
-foundFunctionStorageAccountName=false
-foundFunctionStorageAccountSku=false
-foundFunctionConsumptionPlanRegion=false
-foundFunctionName=false
-foundFunctionRuntime=false
-for var in "$@"
-do
-    echo "    param: $var"
+echo "Gathering pipeline variables"
+resourceGroupName=$RESOURCEGROUPNAME
+echo "    resourceGroupName: $resourceGroupName"
+subscriptionName=$SUBSCRIPTIONNAME
+echo "    subscriptionName: $subscriptionName"
+resourceGroupRegion=$RESOURCEGROUPREGION
+echo "    resourceGroupRegion: $resourceGroupRegion"
+storageAccountRegion=$STORAGEACCOUNTREGION
+echo "    storageAccountRegion: $storageAccountRegion"
+storageAccountName=$STORAGEACCOUNTNAME
+echo "    storageAccountName: $storageAccountName"
+storageAccountSku=$STORAGEACCOUNTSKU
+echo "    storageAccountSku: $storageAccountSku"
+errorDocumentName=$ERRORDOCUMENTNAME
+echo "    errorDocumentName: $errorDocumentName"
+indexDocumentName=$INDEXDOCUMENTNAME
+echo "    indexDocumentName: $indexDocumentName"
+cosmosAccountName=$COSMOSACCOUNTNAME
+echo "    cosmosAccountName: $cosmosAccountName"
+cosmosRegion=$COSMOSREGION
+echo "    cosmosRegion: $cosmosRegion"
+cosmosDbName=$COSMOSDBNAME
+echo "    cosmosDbName: $cosmosDbName"
+cosmosContainerName=$COSMOSCONTAINERNAME
+echo "    cosmosContainerName: $cosmosContainerName"
+cosmosThroughput=$COSMOSTHROUGHPUT
+echo "    cosmosThroughput: $cosmosThroughput"
 
-    # set variable values
-    if [ $foundResourceGroupName = true ] ;
-    then 
-        echo "        setting value for resourceGroupName: $var"
-        resourceGroupName=$var
-        foundResourceGroupName=false
-    elif [ $foundSubscriptionName = true ] ;
-    then
-        echo "        setting value for subscriptionName: $var"
-        subscriptionName=$var
-        foundSubscriptionName=false
-    elif [ $foundResourceGroupRegion = true ];
-    then
-        echo "        setting value for resourceGroupRegion: $var"
-        resourceGroupRegion=$var
-        foundResourceGroupRegion=false
-    elif [ $foundStorageAccountName = true ];
-    then
-        echo "        setting value for storageAccountName: $var"
-        storageAccountName=$var
-        foundStorageAccountName=false
-    elif [ $foundStorageAccountRegion = true ];
-    then
-        echo "        setting value for storageAccountRegion: $var"
-        storageAccountRegion=$var
-        foundStorageAccountRegion=false
-    elif [ $foundStorageAccountSku = true ];
-    then
-        echo "        setting value for storageAccountSku: $var"
-        storageAccountSku=$var
-        foundStorageAccountSku=false
-    elif [ $foundErrorDocumentName = true ];
-    then
-        echo "        setting value for errorDocumentName: $var"
-        errorDocumentName=$var
-        foundErrorDocumentName=false
-    elif [ $foundIndexDocumentName = true ];
-    then
-        echo "        setting value for indexDocumentName: $var"
-        indexDocumentName=$var
-        foundIndexDocumentName=false
-    elif [ $foundCosmosAccountName = true ];
-    then
-        echo "        setting value for cosmosAccountName: $var"
-        cosmosAccountName=$var
-        foundCosmosAccountName=false
-    elif [ $foundCosmosRegion = true ];
-    then
-        echo "        setting value for cosmosRegion: $var"
-        cosmosRegion=$var
-        foundCosmosRegion=false
-    elif [ $foundCosmosDbName = true ];
-    then
-        echo "        setting value for cosmosDbName: $var"
-        cosmosDbName=$var
-        foundCosmosDbName=false
-    elif [ $foundCosmosContainerName = true ];
-    then
-        echo "        setting value for cosmosContainerName: $var"
-        cosmosContainerName=$var
-        foundCosmosContainerName=false
-    elif [ $foundCosmosThroughput = true ];
-    then
-        echo "        setting value for cosmosThroughput: $var"
-        cosmosThroughput=$var
-        foundCosmosThroughput=false
-    elif [ $foundFunctionStorageAccountRegion = true ];
-    then
-        echo "        setting value for functionStorageAccountRegion: $var"
-        functionStorageAccountRegion=$var
-        foundFunctionStorageAccountRegion=false
-    elif [ $foundFunctionStorageAccountName = true ];
-    then
-        echo "        setting value for functionStorageAccountName: $var"
-        functionStorageAccountName=$var
-        foundFunctionStorageAccountName=false
-    elif [ $foundFunctionStorageAccountSku = true ];
-    then
-        echo "        setting value for functionStorageAccountSku: $var"
-        functionStorageAccountSku=$var
-        foundFunctionStorageAccountSku=false
-    elif [ $foundFunctionConsumptionPlanRegion = true ];
-    then
-        echo "        setting value for functionConsumptionPlanRegion: $var"
-        functionConsumptionPlanRegion=$var
-        foundFunctionConsumptionPlanRegion=false
-    elif [ $foundFunctionName = true ];
-    then
-        echo "        setting value for functionName: $var"
-        functionName=$var
-        foundFunctionName=false
-    elif [ $foundFunctionRuntime = true ];
-    then
-        echo "        setting value for functionRuntime: $var"
-        functionRuntime=$var
-        foundFunctionRuntime=false
-    fi
+# echo "Checking environment variable for resource group name: $RESOURCEGROUPNAME"
+# echo "Iterating through parameters..."
+# echo "Number of parameters passed in: $#"
+# foundResourceGroupName=false
+# foundSubscriptionName=false
+# foundResourceGroupRegion=false
+# foundStorageAccountName=false
+# foundStorageAccountRegion=false
+# foundStorageAccountSku=false
+# foundErrorDocumentName=false
+# foundIndexDocumentName=false
+# foundCosmosAccountName=false
+# foundCosmosRegion=false
+# foundCosmosDbName=false
+# foundCosmosContainerName=false
+# foundCosmosThroughput=false
+# foundFunctionStorageAccountRegion=false
+# foundFunctionStorageAccountName=false
+# foundFunctionStorageAccountSku=false
+# foundFunctionConsumptionPlanRegion=false
+# foundFunctionName=false
+# foundFunctionRuntime=false
+# for var in "$@"
+# do
+#     echo "    param: $var"
+
+#     # set variable values
+#     if [ $foundResourceGroupName = true ] ;
+#     then 
+#         echo "        setting value for resourceGroupName: $var"
+#         resourceGroupName=$var
+#         foundResourceGroupName=false
+#     elif [ $foundSubscriptionName = true ] ;
+#     then
+#         echo "        setting value for subscriptionName: $var"
+#         subscriptionName=$var
+#         foundSubscriptionName=false
+#     elif [ $foundResourceGroupRegion = true ];
+#     then
+#         echo "        setting value for resourceGroupRegion: $var"
+#         resourceGroupRegion=$var
+#         foundResourceGroupRegion=false
+#     elif [ $foundStorageAccountName = true ];
+#     then
+#         echo "        setting value for storageAccountName: $var"
+#         storageAccountName=$var
+#         foundStorageAccountName=false
+#     elif [ $foundStorageAccountRegion = true ];
+#     then
+#         echo "        setting value for storageAccountRegion: $var"
+#         storageAccountRegion=$var
+#         foundStorageAccountRegion=false
+#     elif [ $foundStorageAccountSku = true ];
+#     then
+#         echo "        setting value for storageAccountSku: $var"
+#         storageAccountSku=$var
+#         foundStorageAccountSku=false
+#     elif [ $foundErrorDocumentName = true ];
+#     then
+#         echo "        setting value for errorDocumentName: $var"
+#         errorDocumentName=$var
+#         foundErrorDocumentName=false
+#     elif [ $foundIndexDocumentName = true ];
+#     then
+#         echo "        setting value for indexDocumentName: $var"
+#         indexDocumentName=$var
+#         foundIndexDocumentName=false
+#     elif [ $foundCosmosAccountName = true ];
+#     then
+#         echo "        setting value for cosmosAccountName: $var"
+#         cosmosAccountName=$var
+#         foundCosmosAccountName=false
+#     elif [ $foundCosmosRegion = true ];
+#     then
+#         echo "        setting value for cosmosRegion: $var"
+#         cosmosRegion=$var
+#         foundCosmosRegion=false
+#     elif [ $foundCosmosDbName = true ];
+#     then
+#         echo "        setting value for cosmosDbName: $var"
+#         cosmosDbName=$var
+#         foundCosmosDbName=false
+#     elif [ $foundCosmosContainerName = true ];
+#     then
+#         echo "        setting value for cosmosContainerName: $var"
+#         cosmosContainerName=$var
+#         foundCosmosContainerName=false
+#     elif [ $foundCosmosThroughput = true ];
+#     then
+#         echo "        setting value for cosmosThroughput: $var"
+#         cosmosThroughput=$var
+#         foundCosmosThroughput=false
+#     elif [ $foundFunctionStorageAccountRegion = true ];
+#     then
+#         echo "        setting value for functionStorageAccountRegion: $var"
+#         functionStorageAccountRegion=$var
+#         foundFunctionStorageAccountRegion=false
+#     elif [ $foundFunctionStorageAccountName = true ];
+#     then
+#         echo "        setting value for functionStorageAccountName: $var"
+#         functionStorageAccountName=$var
+#         foundFunctionStorageAccountName=false
+#     elif [ $foundFunctionStorageAccountSku = true ];
+#     then
+#         echo "        setting value for functionStorageAccountSku: $var"
+#         functionStorageAccountSku=$var
+#         foundFunctionStorageAccountSku=false
+#     elif [ $foundFunctionConsumptionPlanRegion = true ];
+#     then
+#         echo "        setting value for functionConsumptionPlanRegion: $var"
+#         functionConsumptionPlanRegion=$var
+#         foundFunctionConsumptionPlanRegion=false
+#     elif [ $foundFunctionName = true ];
+#     then
+#         echo "        setting value for functionName: $var"
+#         functionName=$var
+#         foundFunctionName=false
+#     elif [ $foundFunctionRuntime = true ];
+#     then
+#         echo "        setting value for functionRuntime: $var"
+#         functionRuntime=$var
+#         foundFunctionRuntime=false
+#     fi
 
 
-    # get variable names
-    if [ "$var" = "-resourceGroupName" ]; 
-    then
-        echo "        found parameter resourceGroupName"
-        foundResourceGroupName=true
-    elif [ "$var" = "-subscriptionName" ];
-    then
-        echo "        found parameter subscriptionName"
-        foundSubscriptionName=true
-    elif [ "$var" = "-resourceGroupRegion" ];
-    then
-        echo "        found parameter resourceGroupRegion"
-        foundResourceGroupRegion=true;
-    elif [ "$var" = "-storageAccountName" ];
-    then
-        echo "        found parameter storageAccountName"
-        foundStorageAccountName=true
-    elif [ "$var" = "-storageAccountRegion" ];
-    then
-        echo "        found parameter storageAccountRegion"
-        foundStorageAccountRegion=true
-    elif [ "$var" = "-storageAccountSku" ];
-    then
-        echo "        found parameter storageAccountSku"
-        foundStorageAccountSku=true;
-    elif [ "$var" = "-errorDocumentName" ];
-    then
-        echo "        found parameter errorDocumentName"
-        foundErrorDocumentName=true
-    elif [ "$var" = "-indexDocumentName" ];
-    then
-        echo "        found parameter indexDocumentName"
-        foundIndexDocumentName=true;
-    elif [ "$var" = "-cosmosAccountName" ];
-    then
-        echo "        found parameter cosmosAccountName"
-        foundCosmosAccountName=true;
-    elif [ "$var" = "-cosmosRegion" ];
-    then
-        echo "        found parameter cosmosRegion"
-        foundCosmosRegion=true
-    elif [ "$var" = "-cosmosDbName" ];
-    then
-        echo "        found parameter cosmosDbName"
-        foundCosmosDbName=true
-    elif [ "$var" = "-cosmosContainerName" ];
-    then
-        echo "        found parameter cosmosContainerName"
-        foundCosmosContainerName=true
-    elif [ "$var" = "-cosmosThroughput" ];
-    then
-        echo "        found parameter cosmosThroughput"
-        foundCosmosThroughput=true
-    elif [ "$var" = "-functionStorageAccountRegion" ];
-    then
-        echo "        found parameter functionStorageAccountRegion"
-        foundFunctionStorageAccountRegion=true
-    elif [ "$var" = "-functionStorageAccountName" ];
-    then
-        echo "        found parameter functionStorageAccountName"
-        foundFunctionStorageAccountName=true
-    elif [ "$var" = "-functionStorageAccountSku" ];
-    then
-        echo "        found parameter functionStorageAccountSku"
-        foundFunctionStorageAccountSku=true
-    elif [ "$var" = "-functionConsumptionPlanRegion" ];
-    then
-        echo "        found parameter functionConsumptionPlanRegion"
-        foundFunctionConsumptionPlanRegion=true
-    elif [ "$var" = "-functionName" ];
-    then
-        echo "        found parameter functionName"
-        foundFunctionName=true
-    elif [ "$var" = "-functionRuntime" ];
-    then
-        echo "        found parameter functionRuntime"
-        foundFunctionRuntime=true
-    fi
-done
+#     # get variable names
+#     if [ "$var" = "-resourceGroupName" ]; 
+#     then
+#         echo "        found parameter resourceGroupName"
+#         foundResourceGroupName=true
+#     elif [ "$var" = "-subscriptionName" ];
+#     then
+#         echo "        found parameter subscriptionName"
+#         foundSubscriptionName=true
+#     elif [ "$var" = "-resourceGroupRegion" ];
+#     then
+#         echo "        found parameter resourceGroupRegion"
+#         foundResourceGroupRegion=true;
+#     elif [ "$var" = "-storageAccountName" ];
+#     then
+#         echo "        found parameter storageAccountName"
+#         foundStorageAccountName=true
+#     elif [ "$var" = "-storageAccountRegion" ];
+#     then
+#         echo "        found parameter storageAccountRegion"
+#         foundStorageAccountRegion=true
+#     elif [ "$var" = "-storageAccountSku" ];
+#     then
+#         echo "        found parameter storageAccountSku"
+#         foundStorageAccountSku=true;
+#     elif [ "$var" = "-errorDocumentName" ];
+#     then
+#         echo "        found parameter errorDocumentName"
+#         foundErrorDocumentName=true
+#     elif [ "$var" = "-indexDocumentName" ];
+#     then
+#         echo "        found parameter indexDocumentName"
+#         foundIndexDocumentName=true;
+#     elif [ "$var" = "-cosmosAccountName" ];
+#     then
+#         echo "        found parameter cosmosAccountName"
+#         foundCosmosAccountName=true;
+#     elif [ "$var" = "-cosmosRegion" ];
+#     then
+#         echo "        found parameter cosmosRegion"
+#         foundCosmosRegion=true
+#     elif [ "$var" = "-cosmosDbName" ];
+#     then
+#         echo "        found parameter cosmosDbName"
+#         foundCosmosDbName=true
+#     elif [ "$var" = "-cosmosContainerName" ];
+#     then
+#         echo "        found parameter cosmosContainerName"
+#         foundCosmosContainerName=true
+#     elif [ "$var" = "-cosmosThroughput" ];
+#     then
+#         echo "        found parameter cosmosThroughput"
+#         foundCosmosThroughput=true
+#     elif [ "$var" = "-functionStorageAccountRegion" ];
+#     then
+#         echo "        found parameter functionStorageAccountRegion"
+#         foundFunctionStorageAccountRegion=true
+#     elif [ "$var" = "-functionStorageAccountName" ];
+#     then
+#         echo "        found parameter functionStorageAccountName"
+#         foundFunctionStorageAccountName=true
+#     elif [ "$var" = "-functionStorageAccountSku" ];
+#     then
+#         echo "        found parameter functionStorageAccountSku"
+#         foundFunctionStorageAccountSku=true
+#     elif [ "$var" = "-functionConsumptionPlanRegion" ];
+#     then
+#         echo "        found parameter functionConsumptionPlanRegion"
+#         foundFunctionConsumptionPlanRegion=true
+#     elif [ "$var" = "-functionName" ];
+#     then
+#         echo "        found parameter functionName"
+#         foundFunctionName=true
+#     elif [ "$var" = "-functionRuntime" ];
+#     then
+#         echo "        found parameter functionRuntime"
+#         foundFunctionRuntime=true
+#     fi
+# done
 echo
 
 # This Sets the subscription identified to be default subscription 
