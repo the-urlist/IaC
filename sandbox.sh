8513cd8a41d0fe29f1e00d90e58837de22519707
abel <abel.wang@gmail.com>
2019-06-19 14:46:38 -0700

    tweaking jq

--- sandbox.sh
+++ sandbox.sh
@@ -1,7 +1,7 @@
 # this lists all dns records from cloudflare
 echo "getting all dns records from cloudflare"
-listDnsResult="$(curl --header "X-Auth-Key: a084cb5ec135f5619f40895e958a138add805" --header "X-Auth-Email: abel.wang@gmail.com" https://api.cloudflare.com/client/v4/zones/3d7a85d315c8ff6541921c7c2bce9abe/dns_records)"
-listDnsResult="$(echo "$listDnsResult" | jq '.')"
+rawListDnsResult="$(curl --header "X-Auth-Key: a084cb5ec135f5619f40895e958a138add805" --header "X-Auth-Email: abel.wang@gmail.com" https://api.cloudflare.com/client/v4/zones/3d7a85d315c8ff6541921c7c2bce9abe/dns_records)"
+listDnsResult="$(echo "$rawListDnsResult" | jq '.')"
 echo "dsn records from cloudflare"
 echo "$listDnsResult"
 echo
@@ -10,7 +10,7 @@ echo
 numEntries="$(echo "$listDnsResult" | jq '.["result"] | length')"
 echo "number of dns entries: $numEntries" 
 
-dnsName="$(echo "$listDnsResult" | jq .["result"][0]["name"])"
+dnsName="$(echo "$rawListDnsResult" | jq '.["result"][0]["name"]')"
 echo "dns name: $dnsname"
 
 
