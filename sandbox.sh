5a3e5b92ceaaec7d0b8d4417deb79274ce4897c3
abel <abel.wang@gmail.com>
2019-06-19 13:01:04 -0700

    playing with jq

--- sandbox.sh
+++ sandbox.sh
@@ -1,6 +1,7 @@
 # this lists all dns records from cloudflare
 echo "getting all dns records from cloudflare"
 listDnsResult="$(curl --header "X-Auth-Key: a084cb5ec135f5619f40895e958a138add805" --header "X-Auth-Email: abel.wang@gmail.com" https://api.cloudflare.com/client/v4/zones/3d7a85d315c8ff6541921c7c2bce9abe/dns_records)"
+listDnsResult="$($listDnsResult | jq '.')"
 echo "dsn records from cloudflare"
 echo "$listDnsResult"
 echo
