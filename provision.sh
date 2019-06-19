5f5c594992af69adf5e11963df8b408c29ae84d5
abel <abel.wang@gmail.com>
2019-06-18 20:08:21 -0700

    added code to create and configure front door

--- provision.sh
+++ provision.sh
@@ -133,3 +133,153 @@ az webapp auth update \
     --twitter-consumer-secret $TWITTERCONSUMERSECRET \
     --allowed-external-redirect-urls $staticWebsiteUrl
 echo
+
+# this addes the front door extension to the azure cli. It's currently in preview
+# hopefully i can remove this soon
+#
+az extension add \
+    --name front-door
+
+# this creates the front door service used to route all of our 
+
+# this grabs the url for the function app
+echo "getting the url to the azure function"
+functionUrl="$(az functionapp config hostname list --resource-group the-urlist-serverless-abel3 --webapp-name theurlistfunction --query [0].name)"
+echo "function url: $functionUrl"
+echo
+
+# this creates the front door service
+#
+echo "creating front door service"
+az network front-door create \
+    --backend-address $functionUrl \
+    --name $FRONTDOORNAME \
+    --resource-group $RESOURCEGROUPNAME \
+    --backend-host-header $DNSNAME
+echo
+
+# this creates the load balancer for front door frontend
+echo "creating load balancer for front door frontend"
+az network front-door load-balancing create \
+    --front-door-name $FRONTDOORNAME \
+    --name frontendLoadBalanceSetting \
+    --resource-group $RESOURCEGROUPNAME \
+    --sample-size 4 \
+    --successful-samples-required 2
+echo
+
+# this creates the health probe for front door frontend
+#
+echo "creating health probe for front door frontend"
+az network front-door probe create \
+    --front-door-name $FRONTDOORNAME \
+    --interval 255 \
+    --name frontendHealthProbe \
+    --path / \
+    --resource-group $RESOURCEGROUPNAME
+echo
+
+# this creates the backend pool frontend
+#
+echo "creating backend pool frontend"
+az network front-door backend-pool create \
+    --address $staticWebsiteUrl \
+    --front-door-name $FRONTDOORNAME \
+    --load-balancing frontendLoadBalanceSetting \
+    --name frontend \
+    --probe frontendHealthProbe \
+    --resource-group $RESOURCEGROUPNAME
+echo
+
+
+# this creates the load balancer for front door backend
+# 
+echo "creating load balancer for front door backend"
+az network front-door load-balancing create \
+    --front-door-name $FRONTDOORNAME \
+    --name backendLoadBalanceSetting \
+    --resource-group $RESOURCEGROUPNAME \
+    --sample-size 4 \
+    --successful-samples-required 2
+echo
+
+# this creates the health probe for front door backend
+#
+echo "creating health probe for front door backend"
+az network front-door probe create \
+    --front-door-name $FRONTDOORNAME \
+    --interval 255 \
+    --name backendHealthProbe \
+    --path / \
+    --resource-group $RESOURCEGROUPNAME
+echo
+
+# this creates the backend pool backend
+#
+echo "creating backend pool backend"
+az network front-door backend-pool create \
+    --address $functionUrl \
+    --front-door-name $FRONTDOORNAME \
+    --load-balancing backendLoadBalanceSetting \
+    --name backend \
+    --probe backendHealthProbe \
+    --resource-group $RESOURCEGROUPNAME
+echo
+
+# this creates a temp routing rule so we can delete the default created 
+# routing rule. have to go through this round about way because a default
+# routing rule gets created. And you can't add another rule with the same
+# endpoint and pattern. And you can't delete a routing rule if there is only 
+# one. So the plan is, create a temp routing rule with a crazy pattern match,
+# then delete the Default Routing Rule, then create my real routing rule, then
+# delete the temp routing rule. There has got to be a better way to do this!
+#
+echo "creating a temp routing rule"
+az network front-door routing-rule create \
+    --front-door-name $FRONTDOORNAME \
+    --frontend-endpoints DefaultFrontendEndpoint \
+    --name tempRoutingRule \
+    --resource-group $RESOURCEGROUPNAME \
+    --route-type Forward \
+    --patterns /abeltemp/* \
+    --backend-pool frontend
+echo
+
+# this deletes the DefaultRoutingRule that was automatically created
+#
+echo "Deleting DefaultRoutingRule"
+az network front-door routing-rule delete \
+    --front-door-name $FRONTDOORNAME \
+    --name DefaultRoutingRule \
+    --resource-group $RESOURCEGROUPNAME
+echo
+
+# this creates the routing rule frontend
+#
+echo "creating routing rule for frontend"
+az network front-door routing-rule create \
+    --front-door-name $FRONTDOORNAME \
+    --frontend-endpoints DefaultFrontendEndpoint \
+    --name frontend \
+    --resource-group $RESOURCEGROUPNAME \
+    --route-type Forward \
+    --accepted-protocols Http Https \
+    --backend-pool frontend \
+    --forwarding-protocol HttpsOnly
+echo
+
+# this deletes the temp routing rule
+echo "Deleting tempRoutingRule"
+az network front-door routing-rule delete \
+    --front-door-name $FRONTDOORNAME \
+    --name tempRoutingRule \
+    --resource-group $RESOURCEGROUPNAME
+echo
+
+# this deletes the DefaultBackendPool
+echo "Deleting DefaultBackendPool"
+az network front-door backend-pool delete \
+    --front-door-name $FRONTDOORNAME \
+    --name DefaultBackendPool \
+    --resource-group $RESOURCEGROUPNAME
+echo
