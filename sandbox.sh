865f540c86bdc73ee272440926d019d03c0445a7
abel <abel.wang@gmail.com>
2019-06-19 19:08:30 -0700

    tweaking jp stuff

--- sandbox.sh
+++ sandbox.sh
@@ -30,17 +30,7 @@ echo "found dns entry: $foundDnsEntry"
 if [ $foundDnsEntry = true ] ;
 then
     echo "updating dns entry"
-    curl \
-        -X PUT "https://api.cloudflare.com/client/v4/zones/3d7a85d315c8ff6541921c7c2bce9abe/dns_records/da001a069cfd3ab2c98462eefd7f144b" \
-        -H "X-Auth-Email: abel.wang@gmail.com" \
-        -H "X-Auth-Key: a084cb5ec135f5619f40895e958a138add805" \
-        -H "Content-Type: application/json" \
-        --data '{ \
-            "type":"CNAME", \
-            "name":"www", \
-            "content":"abelurlistfd.azurefd.net", \
-            "proxied":false \
-        }'
+    curl -X PUT "https://api.cloudflare.com/client/v4/zones/3d7a85d315c8ff6541921c7c2bce9abe/dns_records/da001a069cfd3ab2c98462eefd7f144b" -H "X-Auth-Email: abel.wang@gmail.com" -H "X-Auth-Key: a084cb5ec135f5619f40895e958a138add805" -H "Content-Type: application/json" --data '{"type":"CNAME", "name":"www", "content":"abelurlistfd.azurefd.net", "proxied":false}'
 else
     echo "adding new dns entry"
     addNewDnsEntryResult="$(curl -X POST "https://api.cloudflare.com/client/v4/zo2bce9abe/dns_records" -H "X-Auth-Email: abel.wang@gmail.com" -H "X-Auth-Key: a084cb5ec135f5619f40895e958a138add805" -H "Content-Type: application/json" --data '{"type":"CNAME", "name":"testdata", "content":"abelurlistfd.azurefd.net", "priority":10, "proxied":false}')"
