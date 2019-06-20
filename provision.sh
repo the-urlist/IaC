fb6f827d613b32f5ac261065484986c244709ad9
abel <abel.wang@gmail.com>
2019-06-19 20:24:25 -0700

    added code to add or update dns to the real provision script

--- provision.sh
+++ provision.sh
@@ -274,6 +274,7 @@ az network front-door routing-rule create \
 echo
 
 # this deletes the temp routing rule
+#
 echo "Deleting tempRoutingRule"
 az network front-door routing-rule delete \
     --front-door-name $FRONTDOORNAME \
@@ -282,6 +283,7 @@ az network front-door routing-rule delete \
 echo
 
 # this deletes the DefaultBackendPool
+#
 echo "Deleting DefaultBackendPool"
 az network front-door backend-pool delete \
     --front-door-name $FRONTDOORNAME \
@@ -290,8 +292,69 @@ az network front-door backend-pool delete \
 echo
 
 # this lists all dns records from cloudflare
+#
 echo "getting all dns records from cloudflare"
-listDnsResult="$(curl --header "X-Auth-Key: $CLOUDFLAREKEY" --header "X-Auth-Email: $CLOUDFLAREEMAIL" https://api.cloudflare.com/client/v4/zones/3d7a85d315c8ff6541921c7c2bce9abe/dns_records)"
+listDnsResult="$(curl \
+    --header "X-Auth-Key: $CLOUDFLAREKEY" \
+    --header "X-Auth-Email: $CLOUDFLAREEMAIL" \
+    https://api.cloudflare.com/client/v4/zones/$CLOUDFLAREZONE/dns_records)"
+listDnsResult="$(echo "$listDnsResult" | jq '.')"
 echo "dsn records from cloudflare"
 echo "$listDnsResult"
-echo
+echo ""
+
+# this parses the number of dns entries from the response using jq
+#
+echo "getting number of dns entries at cloudflare"
+numEntries="$(echo "$listDnsResult" | jq '.["result"] | length')"
+echo "number of dns entries: $numEntries" 
+echo ""
+
+# this looks for our dns name, see if it has been set or not
+#
+foundDnsEntry=false
+foundDnsEntryId="x"
+for (( i=0; i<$numEntries; i++))
+do
+    dnsEntryName="$(echo $listDnsResult | jq '.["result"]['$i']["name"]')"
+    # this strips off the begin and end quotes
+    dnsEntryName="$(sed -e 's/^"//' -e 's/"$//' <<<"$dnsEntryName")"
+    if [  $dnsEntryName = "www.abelurlist.club" ] ;
+    then
+        foundDnsEntry=true
+        foundDnsEntryId="$(echo $listDnsResult | jq '.["result"]['$i']["id"]')"
+        # this strips off the begin and end quotes
+        foundDnsEntryId="$(sed -e 's/^"//' -e 's/"$//' <<<"$foundDnsEntryId")"
+        break
+    fi
+done
+echo "found dns entry: $foundDnsEntry"
+echo "dns entry id: $foundDnsEntryId"
+echo ""
+
+# this either updates or adds a new dns entry to cloudflare
+#
+if [ $foundDnsEntry = true ] ;
+then
+    echo "updating dns entry"
+    curlResponse="$(curl \
+        -X PUT "https://api.cloudflare.com/client/v4/zones/$CLOUDFLAREZONE/dns_records/$foundDnsEntryId" \
+        -H "X-Auth-Email: $CLOUDFLAREEMAIL" \
+        -H "X-Auth-Key: $CLOUDFLAREKEY" \
+        -H "Content-Type: application/json" \
+        --data '{"type":"CNAME", "name":"www", "content":"abelurlistfd.azurefd.net", "proxied":false}')"
+    echo "cloudflare response: "
+    echo "$curlResponse"
+    echo ""
+else
+    echo "adding new dns entry"
+    curlResponse="$(curl \
+        -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLAREZONE/dns_records" \
+        -H "X-Auth-Email: $CLOUDFLAREEMAIL" \
+        -H "X-Auth-Key: $CLOUDFLAREZONE" \
+        -H "Content-Type: application/json" \
+        --data '{"type":"CNAME", "name":"www", "content":"abelurlistfd.azurefd.net", "priority":10, "proxied":false}')"
+    echo "    cloudflare response: "
+    echo $curlResponse
+    echo ""
+fi
