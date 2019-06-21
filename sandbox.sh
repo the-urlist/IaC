1427c063c18a35542ce85b0e4a1d16077139fbcb
abel <abel.wang@gmail.com>
2019-06-21 08:37:00 -0700

    tweaking app insight

--- sandbox.sh
+++ sandbox.sh
@@ -1,11 +1,14 @@
-az resource create \
+appInsightCreateResponse="$(az resource create \
     --resource-group the-urlist-serverless-abel3 \
     --resource-type "Microsoft.Insights/components" \
     --name abelurlistfunctionappinsight \
     --location southcentralus \
-    --properties '{"Application_Type":"web"}' \
-| grep -Po "\"InstrumentationKey\": \K\".*\"" \
-| xargs -I % az functionapp config appsettings set \
-    --name abelurlistfunctionappinsight \
-    --resource-group the-urlist-serverless-abel3 \
-    --settings "APPINSIGHTS_INSTRUMENTATIONKEY = %"
+    --properties '{"Application_Type":"web"}')" 
+    
+    
+#     \
+# | grep -Po "\"InstrumentationKey\": \K\".*\"" \
+# | xargs -I % az functionapp config appsettings set \
+#     --name abelurlistfunctionappinsight \
+#     --resource-group the-urlist-serverless-abel3 \
+#     --settings "APPINSIGHTS_INSTRUMENTATIONKEY = %"
