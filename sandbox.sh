606021781c8ba64814074c4084e4ddff7b1f93e5
abel <abel.wang@gmail.com>
2019-06-21 08:58:06 -0700

    wiring app insight to function

--- sandbox.sh
+++ sandbox.sh
@@ -1,3 +1,5 @@
+# this creates an instance of appliction insight
+echo "creating application insight for the function"
 appInsightCreateResponse="$(az resource create \
     --resource-group the-urlist-serverless-abel3 \
     --resource-type "Microsoft.Insights/components" \
@@ -5,11 +7,12 @@ appInsightCreateResponse="$(az resource create \
     --location southcentralus \
     --properties '{"Application_Type":"web"}')" 
 echo "$appInsightCreateResponse"
-    
-    
-#     \
-# | grep -Po "\"InstrumentationKey\": \K\".*\"" \
-# | xargs -I % az functionapp config appsettings set \
-#     --name abelurlistfunctionappinsight \
-#     --resource-group the-urlist-serverless-abel3 \
-#     --settings "APPINSIGHTS_INSTRUMENTATIONKEY = %"
+echo ""
+
+# this wires up application insights to the function
+echo "wiring up app insight to function"
+echo appInsightCreateResponse | grep -Po "\"InstrumentationKey\": \K\".*\"" \
+    | xargs -I % az functionapp config appsettings set \
+    --name abelurlistfunctionappinsight \
+    --resource-group the-urlist-serverless-abel3 \
+    --settings "APPINSIGHTS_INSTRUMENTATIONKEY = %"
