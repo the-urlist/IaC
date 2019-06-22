938a9ac353af8e998294a6e3738b9c306fb5bcc2
abel <abel.wang@gmail.com>
2019-06-22 15:24:37 -0700

    removed everything except creating FD and then creating frontend host

--- provisionFrontDoor.sh
+++ provisionFrontDoor.sh
@@ -26,157 +26,159 @@ az network front-door create \
     --backend-host-header $IAC_DNSNAME
 echo ""
 
-# this creates the Frontend Host 
+# this creates the Frontend Host for domain name
 #
-echo "creating frontend host www.abelurlist.club"
-
-
-
-
-# this creates the load balancer for front door frontend
-echo "creating load balancer for front door frontend"
-az network front-door load-balancing create \
+echo "creating frontend host domain name: $IAC_DNSNAME"
+az network front-door frontend-endpoint create \
     --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
-    --name frontendLoadBalanceSetting \
-    --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
-    --sample-size 4 \
-    --successful-samples-required 2
-echo ""
+    --host-name $IAC_DNSNAME \
+    --resource-group IAC_EXCLUSIVE_RESOURCEGROUPNAME \
+    --name $IAC_FRIENDLYDNSNAME
 
-# this creates the health probe for front door frontend
-#
-echo "creating health probe for front door frontend"
-az network front-door probe create \
-    --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
-    --interval 255 \
-    --name frontendHealthProbe \
-    --path / \
-    --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME
-echo ""
+# # this creates the load balancer for front door frontend
+# echo "creating load balancer for front door frontend"
+# az network front-door load-balancing create \
+#     --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
+#     --name frontendLoadBalanceSetting \
+#     --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
+#     --sample-size 4 \
+#     --successful-samples-required 2
+# echo ""
 
-# this creates the backend pool frontend
-#
-echo "creating backend pool frontend"
-staticWebsiteUrl="$(az storage account show -n $IAC_EXCLUSIVE_WEBSTORAGEACCOUNTNAME -g $IAC_EXCLUSIVE_RESOURCEGROUPNAME --query "primaryEndpoints.web" --output tsv)"
-echo "static website storage's primary endpoint: $staticWebsiteUrl"
-fqdnStaticWebsite="$(awk -F/ '{print $3}' <<<$staticWebsiteUrl)"
-echo "    fqdn of static website: $fqdnStaticWebsite"
-az network front-door backend-pool create \
-    --address $fqdnStaticWebsite \
-    --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
-    --load-balancing frontendLoadBalanceSetting \
-    --name frontend \
-    --probe frontendHealthProbe \
-    --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME
-echo ""
+# # this creates the health probe for front door frontend
+# #
+# echo "creating health probe for front door frontend"
+# az network front-door probe create \
+#     --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
+#     --interval 255 \
+#     --name frontendHealthProbe \
+#     --path / \
+#     --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME
+# echo ""
+
+# # this creates the backend pool frontend
+# #
+# echo "creating backend pool frontend"
+# staticWebsiteUrl="$(az storage account show -n $IAC_EXCLUSIVE_WEBSTORAGEACCOUNTNAME -g $IAC_EXCLUSIVE_RESOURCEGROUPNAME --query "primaryEndpoints.web" --output tsv)"
+# echo "static website storage's primary endpoint: $staticWebsiteUrl"
+# fqdnStaticWebsite="$(awk -F/ '{print $3}' <<<$staticWebsiteUrl)"
+# echo "    fqdn of static website: $fqdnStaticWebsite"
+# az network front-door backend-pool create \
+#     --address $fqdnStaticWebsite \
+#     --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
+#     --load-balancing frontendLoadBalanceSetting \
+#     --name frontend \
+#     --probe frontendHealthProbe \
+#     --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME
+# echo ""
 
 
-# this creates the load balancer for front door backend
-# 
-echo "creating load balancer for front door backend"
-az network front-door load-balancing create \
-    --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
-    --name backendLoadBalanceSetting \
-    --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
-    --sample-size 4 \
-    --successful-samples-required 2
-echo ""
+# # this creates the load balancer for front door backend
+# # 
+# echo "creating load balancer for front door backend"
+# az network front-door load-balancing create \
+#     --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
+#     --name backendLoadBalanceSetting \
+#     --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
+#     --sample-size 4 \
+#     --successful-samples-required 2
+# echo ""
 
-# this creates the health probe for front door backend
-#
-echo "creating health probe for front door backend"
-az network front-door probe create \
-    --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
-    --interval 255 \
-    --name backendHealthProbe \
-    --path / \
-    --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME
-echo ""
+# # this creates the health probe for front door backend
+# #
+# echo "creating health probe for front door backend"
+# az network front-door probe create \
+#     --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
+#     --interval 255 \
+#     --name backendHealthProbe \
+#     --path / \
+#     --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME
+# echo ""
 
-# this creates the backend pool backend
-#
-echo "creating backend pool backend"
-az network front-door backend-pool create \
-    --address $functionUrl \
-    --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
-    --load-balancing backendLoadBalanceSetting \
-    --name backend \
-    --probe backendHealthProbe \
-    --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME
-echo ""
+# # this creates the backend pool backend
+# #
+# echo "creating backend pool backend"
+# az network front-door backend-pool create \
+#     --address $functionUrl \
+#     --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
+#     --load-balancing backendLoadBalanceSetting \
+#     --name backend \
+#     --probe backendHealthProbe \
+#     --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME
+# echo ""
 
-# this creates a temp routing rule so we can delete the default created 
-# routing rule. have to go through this round about way because a default
-# routing rule and a default backend pool gets created. And you can't add another rule with the same
-# endpoint and pattern. And you can't delete a routing rule if there is only 
-# one. So the plan is, create a temp routing rule with a crazy pattern match,
-# then delete the Default Routing Rule, then create my real routing rule, then
-# delete the temp routing rule. And finally, delete the default back end pool.
-# There has got to be a better way to do this!
-#
-echo "creating a temp routing rule"
-az network front-door routing-rule create \
-    --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
-    --frontend-endpoints DefaultFrontendEndpoint \
-    --name tempRoutingRule \
-    --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
-    --route-type Forward \
-    --patterns /abeltemp/* \
-    --backend-pool frontend
-echo ""
+# # this creates a temp routing rule so we can delete the default created 
+# # routing rule. have to go through this round about way because a default
+# # routing rule and a default backend pool gets created. And you can't add another rule with the same
+# # endpoint and pattern. And you can't delete a routing rule if there is only 
+# # one. So the plan is, create a temp routing rule with a crazy pattern match,
+# # then delete the Default Routing Rule, then create my real routing rule, then
+# # delete the temp routing rule. And finally, delete the default back end pool.
+# # There has got to be a better way to do this!
+# #
+# echo "creating a temp routing rule"
+# az network front-door routing-rule create \
+#     --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
+#     --frontend-endpoints DefaultFrontendEndpoint \
+#     --name tempRoutingRule \
+#     --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
+#     --route-type Forward \
+#     --patterns /abeltemp/* \
+#     --backend-pool frontend
+# echo ""
 
-# this deletes the DefaultRoutingRule that was automatically created
-#
-echo "Deleting DefaultRoutingRule"
-az network front-door routing-rule delete \
-    --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
-    --name DefaultRoutingRule \
-    --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME
-echo ""
+# # this deletes the DefaultRoutingRule that was automatically created
+# #
+# echo "Deleting DefaultRoutingRule"
+# az network front-door routing-rule delete \
+#     --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
+#     --name DefaultRoutingRule \
+#     --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME
+# echo ""
 
-# this creates the routing rule frontend
-#
-echo "creating routing rule for frontend"
-az network front-door routing-rule create \
-    --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
-    --frontend-endpoints DefaultFrontendEndpoint \
-    --name frontend \
-    --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
-    --route-type Forward \
-    --accepted-protocols Http Https \
-    --backend-pool frontend \
-    --forwarding-protocol HttpsOnly
-echo ""
+# # this creates the routing rule frontend
+# #
+# echo "creating routing rule for frontend"
+# az network front-door routing-rule create \
+#     --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
+#     --frontend-endpoints DefaultFrontendEndpoint \
+#     --name frontend \
+#     --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
+#     --route-type Forward \
+#     --accepted-protocols Http Https \
+#     --backend-pool frontend \
+#     --forwarding-protocol HttpsOnly
+# echo ""
 
-# this deletes the temp routing rule
-#
-echo "Deleting tempRoutingRule"
-az network front-door routing-rule delete \
-    --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
-    --name tempRoutingRule \
-    --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME
-echo ""
+# # this deletes the temp routing rule
+# #
+# echo "Deleting tempRoutingRule"
+# az network front-door routing-rule delete \
+#     --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
+#     --name tempRoutingRule \
+#     --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME
+# echo ""
 
-# this deletes the DefaultBackendPool
-#
-echo "Deleting DefaultBackendPool"
-az network front-door backend-pool delete \
-    --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
-    --name DefaultBackendPool \
-    --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME
-echo ""
+# # this deletes the DefaultBackendPool
+# #
+# echo "Deleting DefaultBackendPool"
+# az network front-door backend-pool delete \
+#     --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
+#     --name DefaultBackendPool \
+#     --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME
+# echo ""
 
-# this creates the routing rule api
-#
-echo "creating routing rule for api"
-az network front-door routing-rule create \
-    --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
-    --frontend-endpoints DefaultFrontendEndpoint \
-    --name api \
-    --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
-    --route-type Forward \
-    --accepted-protocols Http Https \
-    --backend-pool backend \
-    --forwarding-protocol HttpsOnly \
-    --patterns /api/*
-echo ""
+# # this creates the routing rule api
+# #
+# echo "creating routing rule for api"
+# az network front-door routing-rule create \
+#     --front-door-name $IAC_EXCLUSIVE_FRONTDOORNAME \
+#     --frontend-endpoints DefaultFrontendEndpoint \
+#     --name api \
+#     --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
+#     --route-type Forward \
+#     --accepted-protocols Http Https \
+#     --backend-pool backend \
+#     --forwarding-protocol HttpsOnly \
+#     --patterns /api/*
+# echo ""
