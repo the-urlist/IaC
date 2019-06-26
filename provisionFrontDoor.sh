ad73ca64338b4070b42b29e4a2e3d8dc31d0717d
abel <abel.wang@gmail.com>
2019-06-26 10:45:46 -0700

    fixed path to arm templates, also instanned front-door extension

--- provisionFrontDoor.sh
+++ provisionFrontDoor.sh
@@ -5,14 +5,20 @@ echo "create Front Door: $IAC_EXCLUSIVE_FRONTDOORNAME"
 az group deployment create \
     --name azuredeployfd \
     --resource-group $IAC_EXCLUSIVE_RESOURCEGROUPNAME \
-    --template-file "$AGENT_RELEASEDIRECTORY/AbelIaCBuild/Drop/frontdoorazuredeploy.json" \
-    --parameters "$AGENT_RELEASEDIRECTORY/AbelIacBuild/Drop/frontdoorazuredeploy.parameters.json" \
+    --template-file "$AGENT_RELEASEDIRECTORY/AbelIaCBuild/drop/frontdoorazuredeploy.json" \
+    --parameters "$AGENT_RELEASEDIRECTORY/AbelIacBuild/drop/frontdoorazuredeploy.parameters.json" \
     --parameters \
         frontDoorName=$IAC_EXCLUSIVE_FRONTDOORNAME \
         backendPool1Address1="$IAC_EXCLUSIVE_WEBSTORAGEACCOUNTNAME.z21.web.core.windows.net" \
         backendPool2Address1="$IAC_EXCLUSIVE_FUNCTIONNAME.azurewebsites.net" \
         customDomainName="$IAC_DNSNAME"
 echo ""
+
+# this addes the front door extension to the azure cli. It's currently in preview
+# hopefully i can remove this soon
+#
+az extension add \
+    --name front-door
 
 # this enables https for the custom domain front end host
 #
